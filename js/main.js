b2c8faad849467f23e53ee65730255f4457b399e
kaliraj <kalisdvk96@gmail.com>
2022-12-21 09:52:06 +0530

    create objects

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let cricket={opener:"R Sharma",middle:"V Kohli",allround:"W Sundar",finisher:"Dhoni",spin:"Y Chahal",bowler:"Buvi"}
+console.log(cricket);
+let mycar={company:"hyundai",model:2009,colour:"Navy blue",engine_diaplacement:"1086cc",fuel_capacity:"35 litres",mileage:"20 kmpl"}
+console.log(mycar);
+let festival={jan:"Pongal",apr:"Ramadan",june:"Bakrid",oct:"Diwali",dec:"New year"}
+console.log(festival);
+let myfav={cricket:"Kohli",actor:"Rajinikanth",music:"Yuvan",director:"lokesh kanagaraj",comedian:"koundamani",actress:"Anushka"}
+console.log(myfav);
+let myself={sslc:2011,hss:2013,engg:2017,work:"quality inspector",experience:"3 years"}
+console.log(myself);
